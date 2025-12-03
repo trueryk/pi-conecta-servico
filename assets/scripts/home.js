9f1ce9e064f9039e8af0aef475461a9b6a0e6d99
trueryk <henrique.jf01@gmail.com>
2025-12-03 18:12:47 -0300

    teste6

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -16,8 +16,8 @@ $(document).ready(function () {
         `);
     } else {
         let currentUser = JSON.parse(localStorage.getItem('currUser'));
-        if (currentUser.tipo.equals('Cliente')) {
-            `
+        if (currentUser.tipo == 'Cliente') {
+            $('#navbar-site-ul').append(`
             <li class="mx-2 mb-1">|</li>
                     <li class="nav-item">
                         <a class="nav-link" href="contratados.html">Meus contratos</a>
@@ -26,9 +26,9 @@ $(document).ready(function () {
                     <li class="nav-item">
                         <a class="nav-link" href="logout.html">Logout</a>
                     </li>
-            `
+            `)
         } else {
-            `
+            $('#navbar-site-ul').append(`
             <li class="mx-2 mb-1">|</li>
                     <li class="nav-item">
                         <a class="nav-link" href="servicos.html">Meus serviços</a>
@@ -37,7 +37,7 @@ $(document).ready(function () {
                     <li class="nav-item">
                         <a class="nav-link" href="logout.html">Logout</a>
                     </li>
-            `
+            `)
         }
     }
 });
